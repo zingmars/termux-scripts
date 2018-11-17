33b47f337459238329fa0a9afae5a772deb0d1f4
Ingmars Melkis <zingmars@users.noreply.github.com>
2018-11-17 10:08:55 +0200

    Added the URL in the success notification.

--- upload.sh
+++ upload.sh
@@ -11,7 +11,7 @@ var=$(curl -# --http1.1 -H "Accept: application/json" -H "Linx-Randomize: yes" -
 
 # Prepare the notification
 url=$(echo "$var" | jq -r '.url') || $(termux-notification -c Upload -t FAILED && exit) # This assumes that the response returns a json with an url parameter. Change this to parse your server's response.
-termux-notification --title Upload --content Success --button1 copy --button1-action "termux-clipboard-set ${url} && termux-toast -s \"URL copied to clipboard\"" --button2 delete --button2-action "termux-notification --id ${notid} --title Upload --content DELETING... && curl -H \"Linx-Delete-Key: xxx\" -X DELETE ${url} && termux-notification --id ${notid} --title Upload --content DELETED" --button3 "open in browser" --button3-action "termux-open-url ${url}" --id "$notid" # The DELETE button sends a DELETE request to the server. You might need to fix that curl request (or even remove it) for your own setup.
+termux-notification --title Upload --content "Success: ${url}" --button1 copy --button1-action "termux-clipboard-set ${url} && termux-toast -s \"URL copied to clipboard\"" --button2 delete --button2-action "termux-notification --id ${notid} --title Upload --content DELETING... && curl -H \"Linx-Delete-Key: xxx\" -X DELETE ${url} && termux-notification --id ${notid} --title Upload --content DELETED" --button3 "open in browser" --button3-action "termux-open-url ${url}" --id "$notid" # The DELETE button sends a DELETE request to the server. You might need to fix that curl request (or even remove it) for your own setup.
 termux-clipboard-set "$url"
 termux-toast -s "URL copied to clipboard."
 
